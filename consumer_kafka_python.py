20a7607ad1b81240c3898222ade143b3785fba6e
Jaime <jatona27@gmail.com>
2022-01-30 18:17:19 +0100

    Cambio el código para que se hagan updates y no se repitan los users

--- consumer_kafka_python.py
+++ consumer_kafka_python.py
@@ -25,9 +25,11 @@ if __name__ == '__main__':
                                          password='r00tpass')
 
     sql_get_user = """SELECT id FROM users WHERE id = %s"""
+    sql_get_user_friend = """SELECT id,friend FROM friends_distance WHERE id = %s AND friend = %s"""
     sql_insert_user = """INSERT INTO users (id,name,last_name,transport,age,gender,cp) Values (%s,%s,%s,%s,%s,%s,%s)"""
     sql_insert_user_friends = """INSERT INTO user_friends (id,friend) Values (%s,%s)"""
     sql_insert_friends_distance = """INSERT INTO friends_distance (id,friend,time,distance,lat,lon) Values (%s,%s,%s,%s,%s,%s)"""
+    sql_update_friends_distance = """UPDATE friends_distance SET time = %s, distance = %s, lat = %s,lon = %s WHERE user = %s AND friend = %s"""
 
     for topic_users in consumer:
         data = json.loads(topic_users.value) #recupero todos los datos
@@ -60,7 +62,7 @@ if __name__ == '__main__':
             #    connection.commit()  # hacemos el commit para que se guarden los datos
             #    cursor.close()
 
-        else:
+        else: #si está dado de alta el usuario, rellenamos sus datos
             time = data["time"]
             distance_frnds = data["distance_frnds"]
             print(data)
@@ -71,11 +73,26 @@ if __name__ == '__main__':
                 print(lat_frnds)
                 lat = lat_frnds[friend]
                 lon = lon_frnds[friend]
-                cursor = connection.cursor(buffered=True)  # abrimos la conexión para registrar los datos
-                insert_data_distance = (id, friend, time, distance,lat,lon)
-                cursor.execute(sql_insert_friends_distance, insert_data_distance)  # insertamos el amigo
-                connection.commit()  # hacemos el commit para que se guarden los datos
-                cursor.close()
+
+                cursor = connection.cursor(buffered=True)  # abrimos la conexión
+                data_tupla = (id,friend)
+                cursor.execute(sql_get_user_friend, data_tupla)  # cerramos la conexión una vez recuperados los datos
+                records = cursor.fetchall()
+
+                if cursor.rowcount == 0:  # si ejecuto la select de búsqueda del usuario y no recupero registros, tendré que registrar ese usuario
+                    cursor.close()
+                    cursor = connection.cursor(buffered=True)  #abrimos la conexión para registrar los datos
+                    insert_data_distance = (id, friend, time, distance,lat,lon)
+                    cursor.execute(sql_insert_friends_distance, insert_data_distance)  #insertamos el amigo
+                    connection.commit()  #hacemos el commit para que se guarden los datos
+                    cursor.close()
+                else:
+                    cursor.close()
+                    cursor = connection.cursor(buffered=True)  # abrimos la conexión para registrar los datos
+                    update_data_distance = (time, distance, lat, lon, id, friend)
+                    cursor.execute(sql_update_friends_distance, update_data_distance)  #ejecutamos el update en caso de que exista la tupla id y friend del usuario
+                    connection.commit()  # hacemos el commit para que se guarden los datos
+                    cursor.close()
 
         cursor.close()
 
